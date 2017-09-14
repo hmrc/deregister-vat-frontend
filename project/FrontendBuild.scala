676d13c95122b5439f8c49df7cb25c89ca329220
William Lea <william.lea@digital.hmrc.gov.uk>
2017-09-14 11:55:31 +0100

    Updated build files

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -1,9 +1,6 @@
 import sbt._
 import play.sbt.PlayImport._
 import play.core.PlayVersion
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object FrontendBuild extends Build with MicroService {
 
@@ -27,8 +24,8 @@ object FrontendBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.8.1" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    "org.jsoup" % "jsoup" % "1.10.2" % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+    "org.mockito" % "mockito-core" % "2.8.47" % scope
   )
-
 }
