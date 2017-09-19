8a4646ba5eec9f0faeaf1395a9a2437f7dd37732
William Lea <william.lea@digital.hmrc.gov.uk>
2017-09-19 10:32:46 +0100

    Updated build file

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import sbt._
 import play.sbt.PlayImport._
@@ -18,9 +33,9 @@ object FrontendBuild extends Build with MicroService {
 
   def test(scope: String = "test") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "2.4.0" % scope,
-    "org.scalatest" %% "scalatest" % "3.0.0" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.1" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.10.2" % scope,
+    "org.jsoup" % "jsoup" % "1.10.3" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope,
     "org.mockito" % "mockito-core" % "2.9.0" % scope
