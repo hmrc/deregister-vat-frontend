95598d3c4601d1ddac15fb22e3bf84146ffc9fb0
Miranda Hawkes <miranda.hawkes@digital.hmrc.gov.uk>
2018-08-03 15:30:40 +0100

    Added AuthoriseAsAgent predicate

---
+++ test/controllers/predicates/AuthoriseAsAgentSpec.scala
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.predicates
+
+import config.ServiceErrorHandler
+import controllers.ControllerBaseSpec
+import org.jsoup.Jsoup
+import play.api.http.Status
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent}
+import play.api.mvc.Results.Ok
+import play.api.test.FakeRequest
+import services.EnrolmentsAuthService
+import uk.gov.hmrc.auth.core.{NoActiveSession, _}
+import uk.gov.hmrc.auth.core.authorise.Predicate
+import uk.gov.hmrc.auth.core.retrieve.{Retrieval, ~}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AuthoriseAsAgentSpec extends ControllerBaseSpec {
+
+  def setup(authResponse: Future[_]): AuthoriseAsAgent = {
+    val mockAuthConnector = mock[AuthConnector]
+
+    (mockAuthConnector.authorise(_: Predicate, _: Retrieval[_])(_: HeaderCarrier, _: ExecutionContext))
+      .expects(*, *, *, *)
+      .returns(authResponse)
+
+    val mockEnrolmentsAuthService: EnrolmentsAuthService = new EnrolmentsAuthService(mockAuthConnector)
+    val serviceErrorHandler = mock[ServiceErrorHandler]
+    new AuthoriseAsAgent(mockEnrolmentsAuthService, serviceErrorHandler, messagesApi, mockConfig)
+  }
+
+  def target(predicate: AuthoriseAsAgent): Action[AnyContent] = {
+    predicate.async {
+      implicit user =>
+        Future.successful(Ok)
+    }
+  }
+
+  "AuthoriseAsAgent" when {
+
+    "calling .invokeBlock" when {
+
+      "the Agent is enrolled to HMRC-AS-AGENT" when {
+
+        "the Agent has delegated authority for the client" should {
+
+          val authResponse = Future.successful(
+            Enrolments(
+              Set(
+                Enrolment(
+                  "HMRC-AS-AGENT",
+                  Seq(EnrolmentIdentifier("AgentReferenceNumber", "XAIT0000000000")),
+                  "Activated",
+                  Some("mtd-vat-auth")
+                )
+              )
+            )
+          )
+
+          lazy val predicate = setup(authResponse)
+          lazy val result = target(predicate)(FakeRequest())
+
+          "return 200" in {
+            status(result) shouldBe Status.OK
+          }
+        }
+
+        "the Agent does not have delegated authority for the client" should {
+
+          val authResponse = Future.failed(InsufficientEnrolments())
+
+          lazy val predicate = setup(authResponse)
+          lazy val result = target(predicate)(FakeRequest())
+
+          //TODO: change to redirect
+          "return 401" in {
+            status(result) shouldBe Status.UNAUTHORIZED
+          }
+
+          //TODO: change to redirect location
+          "render Unauthorised view" in {
+            Jsoup.parse(bodyOf(result)).title shouldBe "Your session has timed out"
+          }
+        }
+      }
+
+      "the Agent is not enrolled to HMRC-AS-AGENT" should {
+
+        val authResponse = Future.successful(
+          Enrolments(
+            Set(
+              Enrolment("OTHER_ENROLMENT",
+                Seq(EnrolmentIdentifier("", "")),
+                "Activated"
+              )
+            )
+          )
+        )
+
+        lazy val predicate = setup(authResponse)
+        lazy val result = target(predicate)(FakeRequest())
+
+        "return 401" in {
+          status(result) shouldBe Status.UNAUTHORIZED
+        }
+
+        "render Session Timeout view" in {
+          //TODO: change to new unauthorised page
+          Jsoup.parse(bodyOf(result)).title shouldBe "Your session has timed out"
+        }
+
+      }
+
+      "there is no active session" should {
+
+        val authResponse = Future.failed(MissingBearerToken())
+        lazy val predicate = setup(authResponse)
+        lazy val result = target(predicate)(FakeRequest())
+
+        "return 401" in {
+          status(result) shouldBe Status.UNAUTHORIZED
+        }
+
+        "render Session Timeout view" in {
+          Jsoup.parse(bodyOf(result)).title shouldBe "Your session has timed out"
+        }
+
+      }
+
+    }
+
+  }
+
+}
