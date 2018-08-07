b43b772372cd3e1fa63a94f54c617f16696185f2
Mitchell Agoma <mitchell.agoma@digital.hmrc.gov.uk>
2018-08-07 14:05:04 +0100

    Updates after PR comments

--- test/views/errors/client/UnauthorisedViewSpec.scala
+++ test/views/errors/client/UnauthorisedViewSpec.scala
@@ -16,22 +16,6 @@
 
 package views.errors.client
 
-/*
- * Copyright 2018 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import assets.messages.{CommonMessages, AgentUnauthorisedPageMessages => Messages}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -50,7 +34,7 @@ class UnauthorisedViewSpec extends ViewBaseSpec {
       val button = "#content .button"
     }
 
-    lazy val view = views.html.errors.client.unenrolled()(request, messages, mockConfig)
+    lazy val view = views.html.errors.client.unauthorised()(request, messages, mockConfig)
     lazy implicit val document: Document = Jsoup.parse(view.body)
 
     s"have the correct document title" in {
