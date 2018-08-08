f7bf34fa5094dfadf4f368ebcf79cc9b8d8d3463
Mitchell Agoma <mitchell.agoma@digital.hmrc.gov.uk>
2018-08-08 09:13:15 +0100

    Removed business debt model - duplicate of one that already existed

--- app/models/BusinessDebt.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json.{Json, OFormat}
-
-case class BusinessDebt(businessInDebt:String)
-
-object BusinessDebt{
-  implicit val format: OFormat[BusinessDebt] = Json.format[BusinessDebt]
-}
