e79e26bf661a4655b53266a16f02affd16183f81
Ladon Jackson <ladon.jackson@digital.hmrc.gov.uk>
2018-08-13 13:36:01 +0100

    [BTAT-3468] added yesNoRevealCondition helper

---
+++ test/views/DeregistrationDateSpec.scala
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import assets.messages.{CommonMessages, DeregistrationDateMessages}
+import forms.{DateForm, DeregistrationDateForm}
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+
+class DeregistrationDateSpec extends ViewBaseSpec {
+
+  object Selectors {
+    val back = ".link-back"
+    val pageHeading = "#content h1"
+    val hint = ".form-hint"
+    val button = ".button"
+    val dayField = "#dateDay"
+    val monthField = "#dateMonth"
+    val yearField = "#dateYear"
+    val dayText = "#deregistrationDate-fieldset > label.form-group.form-group-day > span"
+    val monthText = "#deregistrationDate-fieldset > label.form-group.form-group-month > span"
+    val yearText = "#deregistrationDate-fieldset > label.form-group.form-group-year > span"
+    val errorHeading = "#error-summary-heading"
+    val errorDay = "#deregistrationDateDay-error-summary"
+    val errorMonth = "#deregistrationDateMonth-error-summary"
+    val errorYear = "#deregistrationDateYear-error-summary"
+    val errorField = "#date-fieldset > span.error-notification"
+  }
+
+  "Rendering the Deregistration date page" should {
+
+    lazy val view = views.html.deregistrationDate(DeregistrationDateForm.deregistrationDateForm)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    s"have the correct document title" in {
+      document.title shouldBe DeregistrationDateMessages.title
+    }
+
+    s"have the correct back text" in {
+      elementText(Selectors.back) shouldBe CommonMessages.back
+      element(Selectors.back).attr("href") shouldBe "#"
+    }
+
+    "have no error heading message being displayed" in {
+      document.select(Selectors.errorHeading).isEmpty shouldBe true
+    }
+
+    s"have the correct page heading" in {
+      elementText(Selectors.pageHeading) shouldBe DeregistrationDateMessages.title
+    }
+
+    s"have the correct a radio " in {
+      elementText(Selectors.dayText) shouldBe CommonMessages.day
+      elementText(Selectors.monthText) shouldBe CommonMessages.month
+      elementText(Selectors.yearText) shouldBe CommonMessages.year
+    }
+
+    s"have the correct continue button text and url" in {
+      elementText(Selectors.button) shouldBe CommonMessages.continue
+    }
+
+    "have no error message being displayed for the fields" in {
+      document.select(Selectors.errorField).isEmpty shouldBe true
+    }
+  }
+
+//  "Rendering the Ceased trading date page with missing fields" should {
+//
+//    lazy val view = views.html.deregistrationDate(DeregistrationDateForm.deregistrationDateForm.bind(Map(
+//      "deregistrationDateDay" -> "",
+//      "deregistrationDateMonth" -> "",
+//      "deregistrationDateYear" -> ""
+//    )))
+//    lazy implicit val document: Document = Jsoup.parse(view.body)
+//
+//    s"have the correct document title" in {
+//      document.title shouldBe DeregistrationDateMessages.title
+//    }
+//
+//    s"have the correct back text" in {
+//      elementText(Selectors.back) shouldBe CommonMessages.back
+//      element(Selectors.back).attr("href") shouldBe "#"
+//    }
+//
+//    "have an error heading message being displayed" in {
+//      elementText(Selectors.errorHeading) shouldBe CommonMessages.errorHeading
+//    }
+//
+//    "have an error for each missing field linking to the correct field" in {
+//      elementText(Selectors.errorDay) shouldBe CommonMessages.numericValueError
+//      element(Selectors.errorDay).attr("href") shouldBe Selectors.dayField
+//      elementText(Selectors.errorMonth) shouldBe CommonMessages.numericValueError
+//      element(Selectors.errorMonth).attr("href") shouldBe Selectors.monthField
+//      elementText(Selectors.errorYear) shouldBe CommonMessages.numericValueError
+//      element(Selectors.errorYear).attr("href") shouldBe Selectors.yearField
+//    }
+//
+//    s"have the correct page heading" in {
+//      elementText(Selectors.pageHeading) shouldBe DeregistrationDateMessages.title
+//    }
+//
+//    s"have the correct a radio " in {
+//      elementText(Selectors.dayText) shouldBe CommonMessages.day
+//      elementText(Selectors.monthText) shouldBe CommonMessages.month
+//      elementText(Selectors.yearText) shouldBe CommonMessages.year
+//    }
+//
+//    s"have the correct continue button text and url" in {
+//      elementText(Selectors.button) shouldBe CommonMessages.continue
+//    }
+//
+//    "have an error message being displayed for the fields" in {
+//      elementText(Selectors.errorField) shouldBe CommonMessages.invalidDate
+//    }
+//  }
+}
+
