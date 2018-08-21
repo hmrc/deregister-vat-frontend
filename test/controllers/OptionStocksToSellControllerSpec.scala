6703c721982566291ec962171bc0cb4877a17af4
Mitchell Agoma <mitchell.agoma@digital.hmrc.gov.uk>
2018-08-21 11:42:45 +0100

    Created test controller spec

---
+++ test/controllers/OptionStocksToSellControllerSpec.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.http.Status
+import play.api.mvc.AnyContentAsFormUrlEncoded
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+import scala.concurrent.Future
+
+
+class OptionStocksToSellControllerSpec extends ControllerBaseSpec {
+
+  object TestOptionTaxController extends OptionStocksToSellController(messagesApi, mockAuthPredicate, mockConfig)
+
+  "the user is authorised" when {
+
+    "Calling the .show action" when {
+
+      "the user does not have a pre selected option" should {
+
+        lazy val result = TestOptionTaxController.show()(request)
+
+        "return 200 (OK)" in {
+          mockAuthResult(Future.successful(mockAuthorisedIndividual))
+          status(result) shouldBe Status.OK
+        }
+
+        "return HTML" in {
+          contentType(result) shouldBe Some("text/html")
+          charset(result) shouldBe Some("utf-8")
+        }
+      }
+
+      authChecks(".show", TestOptionTaxController.show(), request)
+    }
+
+    "Calling the .submit action" when {
+
+      "the user submits after selecting an 'Yes' option" should {
+
+        lazy val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+          FakeRequest("POST", "/").withFormUrlEncodedBody(
+            ("yes_no", "yes"),
+            ("amount", "1000.01")
+          )
+        lazy val result = TestOptionTaxController.submit()(request)
+
+        "return 303 (SEE OTHER)" in {
+          mockAuthResult(Future.successful(mockAuthorisedIndividual))
+          status(result) shouldBe Status.SEE_OTHER
+        }
+
+        s"redirect to '${controllers.routes.CapitalAssetsController.show().url}'" in {
+          redirectLocation(result) shouldBe Some(controllers.routes.CapitalAssetsController.show().url)
+        }
+      }
+
+      "the user submits after selecting the 'No' option" should {
+
+        lazy val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+          FakeRequest("POST", "/").withFormUrlEncodedBody(("yes_no", "no"))
+        lazy val result = TestOptionTaxController.submit()(request)
+
+        "return 303 (SEE OTHER)" in {
+          mockAuthResult(Future.successful(mockAuthorisedIndividual))
+          status(result) shouldBe Status.SEE_OTHER
+        }
+
+        s"redirect to '${controllers.routes.CapitalAssetsController.show().url}'" in {
+          redirectLocation(result) shouldBe Some(controllers.routes.CapitalAssetsController.show().url)
+        }
+      }
+
+      "the user submits without selecting an option" should {
+
+        lazy val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+          FakeRequest("POST", "/").withFormUrlEncodedBody(("yes_no", ""))
+        lazy val result = TestOptionTaxController.submit()(request)
+
+        "return 400 (BAD REQUEST)" in {
+          mockAuthResult(Future.successful(mockAuthorisedIndividual))
+          status(result) shouldBe Status.BAD_REQUEST
+        }
+
+        "return HTML" in {
+          contentType(result) shouldBe Some("text/html")
+          charset(result) shouldBe Some("utf-8")
+        }
+      }
+    }
+
+    authChecks(".submit", TestOptionTaxController.submit(), FakeRequest("POST", "/").withFormUrlEncodedBody(("yes_no", "no")))
+  }
+
+}
