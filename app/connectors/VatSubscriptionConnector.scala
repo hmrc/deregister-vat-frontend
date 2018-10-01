d49c39e4039b557d569bb5890b5fde2b34842fcd
Ladon Jackson <ladon.jackson@digital.hmrc.gov.uk>
2018-10-01 13:23:05 +0100

    [BTAT-3872] added VatSubscriptionService and tests

--- app/connectors/VatSubscriptionConnector.scala
+++ app/connectors/VatSubscriptionConnector.scala
@@ -34,9 +34,9 @@ class VatSubscriptionConnector @Inject()(val http: HttpClient,
 
   def url(vrn: String): String = s"${config.vatSubscriptionUrl}/vat-subscription/$vrn/deregister"
 
-  def submit(vrn: String, deregisterModel: DeregistrationInfo)
+  def submit(vrn: String, deregistrationInfoModel: DeregistrationInfo)
             (implicit writes: Writes[DeregistrationInfo], hc: HeaderCarrier, ec: ExecutionContext): Future[Either[ErrorModel, VatSubscriptionResponse]] = {
-    http.PUT[DeregistrationInfo, Either[ErrorModel, VatSubscriptionResponse]](url(vrn), deregisterModel)(writes, VatSubscriptionHttpParser.updateReads, hc, ec)
+    http.PUT[DeregistrationInfo, Either[ErrorModel, VatSubscriptionResponse]](url(vrn), deregistrationInfoModel)(writes, VatSubscriptionHttpParser.updateReads, hc, ec)
   }
 
 }
---
+++ test/services/VatSubscriptionServiceSpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import assets.constants.BaseTestConstants.{vrn, _}
+import connectors.mocks.MockVatSubscriptionConnector
+import models.{DeregisterVatSuccess, VatSubscriptionSuccess}
+import utils.TestUtil
+import assets.constants.DeregistrationInfoTestConstants.deregistrationInfoMinModel
+
+
+class VatSubscriptionServiceSpec extends TestUtil with MockVatSubscriptionConnector {
+
+  object TestVatSubscriptionService extends VatSubscriptionService(vatSubscriptionConnector = mockVatSubscriptionConnector)
+
+  "The VatSubscriptionService" when {
+
+    "Calling the .storeAnswer method" when {
+
+      "a success response is returned from the connector" should {
+
+        "return the expected model" in {
+          setupMockStoreAnswers[TestModel](vrn, deregistrationInfoMinModel)(Right(VatSubscriptionSuccess))
+          await(TestVatSubscriptionService.updateDereg(deregistrationInfoMinModel)) shouldBe Right(VatSubscriptionSuccess)
+        }
+      }
+
+      "an error response is returned form the connector" should {
+
+        "return the expected error model" in {
+          setupMockStoreAnswers[TestModel](vrn, deregistrationInfoMinModel)(Left(errorModel))
+          await(TestVatSubscriptionService.updateDereg(deregistrationInfoMinModel)) shouldBe Left(errorModel)
+        }
+      }
+    }
+  }
+}
