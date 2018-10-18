5cb03d1a7afa7b3413d1a8eabed4e9963749070b
Miranda Hawkes <miranda.hawkes@digital.hmrc.gov.uk>
2018-10-18 10:37:49 +0100

    Improved error logging

--- app/connectors/VatSubscriptionConnector.scala
+++ app/connectors/VatSubscriptionConnector.scala
@@ -21,8 +21,6 @@ import connectors.httpParsers.VatSubscriptionHttpParser
 import javax.inject.{Inject, Singleton}
 import models.deregistrationRequest.DeregistrationInfo
 import models.{ErrorModel, VatSubscriptionResponse}
-import play.api.Logger
-import play.api.libs.json.Json
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
