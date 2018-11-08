c98fbdb5419efca3df0834d8e62b398b5930fea4
Ladon Jackson <ladon.jackson@digital.hmrc.gov.uk>
2018-11-08 11:41:48 +0000

    [BTAT-4336] added all GA tags except change links on checkYourAnswers page

--- public/javascript/deregister.js
+++ public/javascript/deregister.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-    if(typeof GOVUK.ShowHideContent !== 'undefined') {
-        var showHideContent = new GOVUK.ShowHideContent();
-        showHideContent.init();
-     }
- });
+  if(typeof GOVUK.ShowHideContent !== 'undefined') {
+    var showHideContent = new GOVUK.ShowHideContent();
+      showHideContent.init();
+  }
+});
+
+function sendGAEvent(category, action, label) {
+  ga('send', 'event', category, action, label);
+}
+
+$(document).ready($(function () {
+  $('[data-metrics]').each(function () {
+    var metrics = $(this).attr('data-metrics');
+    var parts = splitOnUser[0].split(':');
+    sendGAEvent(parts[0], parts[1], parts[2]);
+  });
+}));
