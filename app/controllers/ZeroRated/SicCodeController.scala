888a8c4bd949ee09cbc926092a5a1611b1b91097
JoshuaGomersallHMRC <53816372+JoshuaGomersallHMRC@users.noreply.github.com>
2019-12-05 10:35:25 +0000

    BTAT-7360 Added new controllers and routes

---
+++ app/controllers/ZeroRated/SicCodeController.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.ZeroRated
+
+import cats.data.EitherT
+import cats.instances.future._
+import config.{AppConfig, ServiceErrorHandler}
+import controllers.predicates.{AuthPredicate, PendingChangesPredicate}
+import forms.DeregistrationReasonForm
+import javax.inject.{Inject, Singleton}
+import models._
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import services._
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+
+import scala.concurrent.Future
+
+@Singleton
+class SicCodeController @Inject()(val messagesApi: MessagesApi,
+                                  val authenticate: AuthPredicate,
+                                  val pendingDeregCheck: PendingChangesPredicate,
+                                  val deregReasonAnswerService: DeregReasonAnswerService,
+                                  val serviceErrorHandler: ServiceErrorHandler,
+                                  implicit val appConfig: AppConfig) extends FrontendController with I18nSupport {
+
+  val show: Action[AnyContent] = (authenticate andThen pendingDeregCheck) { implicit user =>
+    Ok("")
+  }
+
+  val submit: Action[AnyContent] = authenticate { implicit user =>
+    Ok("")
+  }
+}
+
