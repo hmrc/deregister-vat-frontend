e2261a2325f9c6a917746be4b7e61f8fd4e3062c
Sam Raspin <14580184+sraspin@users.noreply.github.com>
2019-12-18 12:27:12 +0000

    [BTAT-7386] - added purchasesExceedSuppliesForm

---
+++ test/forms/PurchasesExceedSuppliesFormSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.PurchasesExceedSuppliesForm._
+import models.{No, Yes}
+import play.api.data.FormError
+import uk.gov.hmrc.play.test.UnitSpec
+
+class PurchasesExceedSuppliesFormSpec extends UnitSpec {
+
+  "PurchasesExceedSuppliesForm" should {
+
+    "successfully parse a Yes" in {
+      val res = purchasesExceedSuppliesForm.bind(Map(yesNo -> YesNoForm.yes))
+      res.value should contain(Yes)
+    }
+
+    "successfully parse a No" in {
+      val res = purchasesExceedSuppliesForm.bind(Map(yesNo -> YesNoForm.no))
+      res.value should contain(No)
+    }
+
+    "fail when nothing has been entered" in {
+      val res = purchasesExceedSuppliesForm.bind(Map.empty[String, String])
+      res.errors should contain(FormError(yesNo, purchasesExceedSupplyError))
+      res.errors.size shouldBe 1
+    }
+
+    "A form built from a valid model" when {
+
+      "'Yes' is selected" should {
+
+        "generate the correct mapping" in {
+          val form = PurchasesExceedSuppliesForm.purchasesExceedSuppliesForm.fill(Yes)
+          form.data shouldBe Map(yesNo -> "yes")
+        }
+      }
+
+      "'No' is selected" should {
+
+        "generate the correct mapping" in {
+          val form = PurchasesExceedSuppliesForm.purchasesExceedSuppliesForm.fill(No)
+          form.data shouldBe Map(yesNo -> "no")
+        }
+      }
+    }
+  }
+}
