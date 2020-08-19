11ec04ee983798bb3a0f21a1b0036cd2dd43b2f1
James Rees <6393840+Jxr227@users.noreply.github.com>
2020-08-19 09:05:58 +0100

    Removed whitelist

--- app/config/ConfigKeys.scala
+++ app/config/ConfigKeys.scala
@@ -24,11 +24,6 @@ object ConfigKeys {
   val googleAnalyticsToken: String = googleAnalyticsRoot + ".token"
   val googleAnalyticsHost: String = googleAnalyticsRoot + ".host"
 
-  val whitelistEnabled: String = "whitelist.enabled"
-  val whitelistedIps: String = "whitelist.allowedIps"
-  val whitelistExcludedPaths: String = "whitelist.excludedPaths"
-  val whitelistShutterPage: String = "whitelist.shutter-page-url"
-
   val signInBaseUrl: String = "signIn.url"
   val signInContinueBaseUrl: String = "signIn.continueBaseUrl"
 
