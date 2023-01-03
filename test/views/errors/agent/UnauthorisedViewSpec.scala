276e846139dde91bd7de7d7f38c94556b1dcdc2d
JoNB7777 <60427113+JoNB7777@users.noreply.github.com>
2023-01-03 12:33:12 +0000

    [BTAT-11201] Updated license header in previously missed file

--- test/views/errors/agent/UnauthorisedViewSpec.scala
+++ test/views/errors/agent/UnauthorisedViewSpec.scala
@@ -17,7 +17,7 @@
 package views.errors.agent
 
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
