57ffed339286afcdc703e1343a9d8508df23b85f
Michele Satta <3033136+msatta@users.noreply.github.com>
2026-04-20 13:59:07 +0100

    Remove Option to Tax example content (#354)
    * Remove Option to Tax example content

--- test/views/OTTNotificationSpec.scala
+++ test/views/OTTNotificationSpec.scala
@@ -31,8 +31,7 @@ class OTTNotificationSpec extends ViewBaseSpec {
     val back = ".govuk-back-link"
     val pageHeading = "#content h1"
     val text = "p:nth-of-type(1)"
-    val inset1 = ".govuk-inset-text > p:nth-of-type(1)"
-    val inset2 = ".govuk-inset-text > p:nth-of-type(2)"
+    val inset = ".govuk-inset-text > p:nth-of-type(1)"
     val yesOption = "fieldset > div > div:nth-of-type(1) > label"
     val noOption = "fieldset > div > div:nth-of-type(2) > label"
     val button = ".govuk-button"
@@ -61,12 +60,8 @@ class OTTNotificationSpec extends ViewBaseSpec {
       elementText(Selectors.text) shouldBe OTTNotificationMessages.text
     }
 
-    "have the correct first inset paragraph" in {
-      elementText(Selectors.inset1) shouldBe OTTNotificationMessages.inset1
-    }
-
-    "have the correct second inset paragraph" in {
-      elementText(Selectors.inset2) shouldBe OTTNotificationMessages.inset2
+    "have the correct inset paragraph" in {
+      elementText(Selectors.inset) shouldBe OTTNotificationMessages.inset
     }
 
     "have the correct a radio button form with yes/no answers" in {
